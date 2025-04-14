8895cd4e5fda4f3646723036aab4f63ad42a3eab
Bruno Reus <breus@frba.utn.edu.ar>
2025-04-14 00:36:01 -0300

    Add files via upload
    Corregi la forma que rediccione la salida

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+   
+    printf("Hello, world!\n"); //Se escribe en la terminal
+
+    //En la terminal escribo "./hello.exe > output.txt"
+    //Para poder redirigir la salida a formato de texto
+    return 0;
+}
